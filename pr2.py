f366abad38f0448c29df91457c41c18f3a26f81f
MinJae <guswlfahs123@naver.com>
2020-04-10 20:14:56 +0900

    pr2.py 각종 문자열, +문자열

---
+++ pr2.py
@@ -0,0 +1,20 @@
+a= 'i \'learn\' python'
+print (a)
+b= " i 'learn' python "
+print (b)
+
+c = """i want to
+learn flutter
+"""
+print (c)
+
+print (len('얼른 하고 플러터 배우자...'))
+
+name = input('What is your name')
+message=name + '반갑습니다!'
+print (message) 
+
+repeat = '반복'*5
+print (repeat)
+
+
