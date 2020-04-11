8d391bfe8256f75ece4fb4dad50b0c459ac0c3c3
MinJae <guswlfahs123@naver.com>
2020-04-11 16:32:46 +0900

    pr10.py 반복문 간단한 예제

---
+++ pr10.py
@@ -0,0 +1,13 @@
+
+
+for num in range(1,10):
+    print ('7 x',num,'=',7*num)
+
+for num in range(1,100):
+    if num%23==0 :
+        print(num)
+
+        
+for num in range(2,5):
+    for a in range (1,6):
+        print(num**a)
